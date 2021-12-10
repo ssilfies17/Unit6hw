803014b2779730b59378781eaed53f4a482f4fb4
Sawyer Silfies <ssilfies17@gmail.com>
2021-12-09 21:48:24 -0800

    javascript further developed and readme added

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unit6hw
 Unit 6 homework - Weather Dashboard
+
+This is my weather dashboard. It provides weather from a list of popular cities, as well as the ability to view weather for any city via the search function.
--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 console.log("hello");
+
+var header = document.querySelector('header');
+
+header.setAttribute("style", "font-size:20px; margin:10px; padding-top:5px; display:flex; justify-content:center; background-color:#DFFF00; color:grey;")
+
